f861bd05d13cfbcf68e4548b8cc4aad57d44b2ea
Sylvain Boily <sylvainboilydroid@gmail.com>
2017-04-06 08:45:32 -0400

    WIP add trunk listing

--- outcall/form.py
+++ outcall/form.py
@@ -4,12 +4,26 @@
 
 from flask_wtf import FlaskForm
 
-from wtforms.fields import SubmitField
-from wtforms.fields import StringField
-
+from wtforms.fields import (SubmitField,
+                            StringField,
+                            SelectField,
+                            FieldList,
+                            FormField)
 from wtforms.validators import InputRequired
+
+
+class OutcallExtensionForm(FlaskForm):
+    context = StringField('Context')
+    exten = StringField('Extension')
+    caller_id = StringField('Caller ID')
+    external_prefix = StringField('External prefix')
+    prefix = StringField('Prefix')
+    strip_digits = StringField('Strip digits')
 
 
 class OutcallForm(FlaskForm):
     name = StringField('Name', [InputRequired()])
+    description = StringField('Description')
+    extensions = FieldList(FormField(OutcallExtensionForm))
+    trunk = SelectField('Trunks', choices=[])
     submit = SubmitField('Submit')
