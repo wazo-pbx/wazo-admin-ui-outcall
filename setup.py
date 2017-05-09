0ad867d740d4307bcc837bf99ca0a159b1b01cd3
Sylvain Boily <sylvainboilydroid@gmail.com>
2017-05-09 15:45:10 -0400

    rename path, add package

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
 
     entry_points={
         'wazo_admin_ui.plugins': [
-            'outcall = outcall.plugin:Plugin',
+            'outcall = wazo_plugind_admin_ui_outcall_official.plugin:Plugin',
         ]
     }
 )
